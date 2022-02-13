0cdee54bd962a4d677fd1b266f4227cacb506c2a
Eduardo Kobus <eduardo.kobus@hotmail.com>
2022-02-13 15:34:33 -0300

    Refatoring Foodfy - Add Button recipes details

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ for (let recipe of recipesCards ) {
     })
 }
 
+
+var btn = document.querySelectorAll(".btn_steps");
+
+btn.addEventListener("click", function() {
+    var invokeShow = document.querySelectorAll(".show_and_hide");
+    
+  if(invokeShow.style.display === "none") {
+        invokeShow.style.display = "block";
+    } else {
+      invokeShow.style.display = "none";
+  }
+    
+});
+
